020296aeb0f3ff8310c005022979db607092d827
nicksolony <nicksolony@gmail.com>
2020-03-03 00:18:54 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,16 @@
 ENV['SINATRA_ENV'] ||= "development"
 
-require 'bundler/setup'
+require 'bundler'
 Bundler.require(:default, ENV['SINATRA_ENV'])
 
-require_all 'app'
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
+
+
+
+require 'sinatra/base'
+require 'sinatra/reloader'
+
+require_all './app'
---
+++ db/migrate/02_create_heroes.rb
@@ -0,0 +1,10 @@
+class CreateHeroes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :heros do |t|
+        t.string :name
+        t.string :power
+        t.string :bio
+        t.integer :team_id
+      end
+    end
+end
